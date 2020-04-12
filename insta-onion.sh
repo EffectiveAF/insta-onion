465c871a0a80efeb9a2403d7c3ec514eb15eef5f
Steve Phillips <steve@tryingtobeawesome.com>
2020-04-11 21:36:33 -0700

    insta-onion.sh: made it more robust + tells user .onion address on success

--- insta-onion.sh
+++ insta-onion.sh
@@ -3,14 +3,14 @@
 set -euo pipefail
 
 if [ $# -lt 2 ]; then
-  echo '    $ sudo bash insta-onion.sh <onion_service_name> <local_http_port_your_app_is_listening_on>'
-  echo
-  echo 'or'
-  echo
-  echo '    # bash insta-onion.sh <onion_service_name> <local_http_port_your_app_is_listening_on>'
-  echo
-  echo 'Note: onion_service_name will be used to create a folder at /var/lib/tor/<onion_service_name>'
-  exit
+    echo '    $ sudo bash insta-onion.sh <onion_service_name> <local_http_port_your_app_is_listening_on>'
+    echo
+    echo 'or'
+    echo
+    echo '    # bash insta-onion.sh <onion_service_name> <local_http_port_your_app_is_listening_on>'
+    echo
+    echo 'Note: onion_service_name will be used to create a folder at /var/lib/tor/<onion_service_name>'
+    exit 1
 fi
 export onion_service_name="${1:-onion_service_`date -I`}"
 export local_http_port="${2:-8000}"
@@ -18,12 +18,12 @@ export local_http_port="${2:-8000}"
 export distro="$(lsb_release -is)"
 if [[ ("$distro" != 'Debian') && ("$distro" != 'Ubuntu') ]]; then
     echo "For now, this script only works on Debian and Ubuntu. Sorry!";
-    exit
+    exit 1
 fi
 
 if [[ "$(lsb_release -cs)" == "trusty" ]]; then
     echo "$(lsb_release -ds) is too old! Consider upgrading."
-    exit
+    exit 1
 fi
 
 sudo apt-get update && sudo apt-get install -y curl gnupg2
@@ -43,15 +43,22 @@ sudo chmod 0700 "$onion_dir"
 echo 'Appending to /etc/tor/torrc ...'
 
 echo "" | sudo tee -a /etc/tor/torrc
-echo "" | sudo tee -a /etc/tor/torrc
 echo "HiddenServiceDir $onion_dir" | sudo tee -a /etc/tor/torrc
 echo "HiddenServicePort 80 127.0.0.1:$local_http_port" | sudo tee -a /etc/tor/torrc
 
 sudo service tor restart
 
+sleep 2
+
+if [[ -f "$onion_dir"/hostname ]]; then
+    echo
+    echo 'Congratulations! Your onion service now exists at '$(cat "$onion_dir"/hostname)
+    exit 0
+fi
+
 echo -n '
 
-...Now just wait a few minutes for your Onion service to register with the Tor network!'
+...Now just wait a few minutes for your Onion service to be created!'
 echo ' Once it has, you can view your new, randomly-generated .onion address by running
 
     $ sudo cat '"$onion_dir"/hostname'
